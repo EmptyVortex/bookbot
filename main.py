73de87f5637fbf787ad6092eb48152590c283290
EmptyVortex <malcolmshire12@gmail.com>
2025-05-23 13:12:56 +0100

    main.py print word count

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+count = 0
+
 def get_book_text(path):
     with open(path) as book:
         return book.read()
         
 
 def main(): 
-    print(get_book_text("./books/frankenstein.txt"))
+    count = 0
+    book = get_book_text("./books/frankenstein.txt")
+    words = book.split()
+    for word in words:
+        count += 1
+    print(f"{count} words found in the document")
 
 main()
