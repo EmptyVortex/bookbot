d7e99abe53d6e7dc0b00b1cf0b00b37a9ffad9ef
EmptyVortex <malcolmshire12@gmail.com>
2025-05-23 17:45:11 +0100

    start dict_sort

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from stats import get_num_words
+from stats import get_letters
+from stats import dict_sort
 
 def main(): 
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
     print(f"{num_words} words found in the document")
+    letter_count = get_letters(book)
+    print(letter_count)
+    dict_sort(letter_count)
     
 
 
--- stats.py
+++ stats.py
@@ -1,4 +1,30 @@
 def get_num_words(book): 
     words = book.split()
     return len(words)
-    
+
+def get_letters(book):
+    character_count = {}
+    words = book.split()
+    character_count[" "] = len(words) -1
+
+    for word in words:
+        letters = list(word)
+        for letter in letters:
+            letter = letter.lower()
+            if letter in character_count:
+                character_count[letter] += 1
+            else: 
+                character_count[letter] = 1
+            
+    return character_count
+
+
+def dict_sort(letter_count):
+    count = 0
+    dict_list = []
+    for i in letter_count:
+        count += 1
+        print(i)
+        print(count)
+
+    return
