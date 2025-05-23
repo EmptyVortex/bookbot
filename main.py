76ed04b90d7c5805d0385a421cbf286b5fce1348
EmptyVortex <malcolmshire12@gmail.com>
2025-05-23 18:55:58 +0100

    use sys.argv input

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from stats import get_num_words
 from stats import get_letters
 from stats import dict_sort
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main(): 
-    book = get_book_text("./books/frankenstein.txt")
+    book = get_book_text(sys.argv[1])
     num_words = get_num_words(book)
     letter_count = get_letters(book)
     letter_list = dict_sort(letter_count)
-    
     print_report(num_words, letter_list)
+    
    
-   
+
 def get_book_text(path):
     with open(path) as book:
         return book.read()
         
 def print_report(num_words, letter_list):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
