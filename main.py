629ff2ae9737b12d2a6727f25c1270b4a2858630
EmptyVortex <malcolmshire12@gmail.com>
2025-05-23 12:55:47 +0100

    main.py print 1st book

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
+        
+
+def main(): 
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
